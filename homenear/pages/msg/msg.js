ec2de1442a7619b2d6d8fa6ed3470f920530940d
wugj <wugaojie@haozu.com>
2017-06-30 16:20:02 +0800

    小程序-头部tab，wxml引入wxml

--- homenear/pages/msg/msg.js
+++ homenear/pages/msg/msg.js
@@ -1,82 +1,74 @@
-// login.js
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    plain:false,
-    loading:false,
-    btn:'加载中',
+    navTab: ["推荐", "圆桌", "热门", "收藏"],
+    currentNavtab: "0"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   },
-
-  // 登录按钮点击事件
-  onclick: function (e) {
-      this.setData({
-        plain:!this.data.plain,
-        loading:!this.data.loading,
-      })
-      wx.navigateTo({
-        url: '../msg/msg',
-      })
+  onLoad: function () {
+    console.log('onLoad')
   },
-
-
+  switchTab: function (e) {
+    this.setData({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
+  }
 })
