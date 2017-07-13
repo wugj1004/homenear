0d983658f41375bc329a5c67004701e31365a732
wugj <wugaojie@haozu.com>
2017-07-13 19:21:03 +0800

    WX-商品详情，商城截图

---
+++ homenear/pages/classfy/list/index.js
@@ -0,0 +1,26 @@
+// index.js
+var app = getApp()
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    console.log("list loadinng ...");
+    var that = this;
+
+    wx.request({
+      url: 'http://www.huanqiuxiaozhen.com/wemall/goods/inqGoodsByTypeBrand?brand=' + options.brand + "&typeid=" + options.typeid,
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        that.setData({
+          list: res.data.data
+        });
+      }
+    })
+  }
+
+})
