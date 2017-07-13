01085fc2f156e1233f1e0c11ffd0f01edbbf602e
wugj <wugaojie@haozu.com>
2017-07-13 16:21:15 +0800

    WX-ui跳转

--- homenear/pages/classfy/classfy.js
+++ homenear/pages/classfy/classfy.js
@@ -28,8 +28,7 @@ Page({
 
   //事件处理函数
   switchRightTab: function (e) {
-    let id = e.target.dataset.id,
-      index = parseInt(e.target.dataset.index);
+    let id = e.target.dataset.id, index = parseInt(e.target.dataset.index);
     this.setData({
       curNav: id,
       curIndex: index
