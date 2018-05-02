6d8ba8c95ed900a553e015b0b6d29ed28d511983
wugj <wugaojie@haozu.com>
2018-05-02 17:38:27 +0800

    添加worker

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // 逻辑层程序注册
 App({
   globalData: 'I am global data',
+
+
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
+  
   onLaunch: function () {
-    
+    var receive = {a:"123",b:12,c:"string"}
+    var supplies = { d: "dedede", e: 22222, c: "测试" }
+    this.extend(receive, supplies,false,true)
   },
 
   /**
@@ -31,5 +36,35 @@ App({
 
   onPageNotFound: function (res){
 
-  }
+  },
+  /**
+     * 合并两个对象。
+     * 将参数supplies对象的元素合并到参数receive对象中
+     * @static
+     * @param {object} receive 源对象
+     * @param {object} supplies 用来合并到receive中的对象
+     * @param {boolean} overwritten 如果设为true，supplies中的元素将替换receive中的同名元素，默认值为true
+     * @param {boolean} recursion 是否递归，默认值为false
+     * @return {object} 返回receive对象的引用
+     */
+  extend: function (r, s, ov, rec) {
+    console.log("打开面板")
+    if (typeof r === "object" && typeof s === "object") {
+      for (var i in s) {
+        if (s.hasOwnProperty(i)) {
+          if (!(i in r)) {
+            r[i] = s[i];
+          } else if (!ov) {
+            if (rec && typeof r[i] === "object" && typeof s[i] === "object") {
+              extend(r[i], s[i], ov, rec);
+            } else {
+              r[i] = s[i];
+            }
+          }
+        }
+      }
+    }
+    return r;
+  },
+
 })
